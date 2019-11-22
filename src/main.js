d22bfc30d4c78367de3cfa5582283b703edc24a8
Masha Ivy <mashanechmo@gmail.com>
2019-11-22 11:20:25 -0800

    add UI that will take user's input

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 //  ui
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { DoctorLookup } from './DoctorLookup.js'
+
+  $("#userInput").submit(function(event) {
+    const location = $("#location").val();
+    (async () => {
+   let bikeIndex = new BikeIndex();
+   const response = await bikeIndex.getBike(city);
+   getElements(response);
+ })();
+  const getElements = function(responce) {
+    $(".result")
+  }
+  })
